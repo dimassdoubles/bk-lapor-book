49b3fc590a6f8f0cfc29ea8f71dfc45b0effe5d3
dimassdoubles <codedimas@gmail.com>
2024-01-05 20:39:42 +0700

    Membuat detail dan ubah status

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lapor_book/firebase_options.dart';
 import 'package:lapor_book/pages/AddFormPage.dart';
+import 'package:lapor_book/pages/DetailPage.dart';
 import 'package:lapor_book/pages/LoginPage.dart';
 import 'package:lapor_book/pages/RegisterPage.dart';
 import 'package:lapor_book/pages/SplashPage.dart';
@@ -23,7 +24,7 @@ void main() async {
       '/register': (context) => const RegisterPage(),
       '/dashboard': (context) => const DashboardPage(),
       '/add': (context) => AddFormPage(),
-      // '/detail': (context) => DetailPage(),
+      '/detail': (context) => DetailPage(),
     },
   ));
 }
